7a081ef977812bb49804efe25cd5c372aa443fc7
frankito42 <59475773+frankito42@users.noreply.github.com>
2025-03-10 00:58:18 -0300

    s

--- conn/conn.php
+++ conn/conn.php
@@ -1,9 +1,13 @@
 <?php
 
 $server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'ogajara';
+/* $server = 'localhost';
 $username = 'u172132148_c';
 $password = '/lh0T4Qs3;';
-$database = 'u172132148_c';
+$database = 'u172132148_c'; */
 
 try {
     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
