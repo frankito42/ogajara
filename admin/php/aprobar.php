384cd7c656b8eba04bdb84bbfd9a9b155c5b337e
frankito42 <59475773+frankito42@users.noreply.github.com>
2025-03-09 23:06:31 -0300

    asd

--- admin/php/aprobar.php
+++ admin/php/aprobar.php
@@ -1,11 +1,7 @@
 <?php
 require "../../conn/conn.php";
 $datexd=date("Y-m-d");
-<<<<<<< HEAD
 $sql="SELECT `id`, `idEstadoCuenta`, `nroCuota`, `total`, `estado`, `vencimiento`, `fechaPago`, `metodoPago`, `verificacion` FROM `estadocuotas` WHERE `idEstadoCuenta`=$_GET[id] and estado=1 and verificacion='pendiente'";
-=======
-$sql="SELECT `id`, `idEstadoCuenta`, `nroCuota`, `total`, `estado`, `vencimiento`, `fechaPago`, `metodoPago`, `verificacion`,u.user FROM `estadocuotas` e JOIN usersadmin u on u.idUser=e.idUser WHERE `idEstadoCuenta`=$_GET[id] and estado=1 and verificacion='pendiente';";
->>>>>>> 5a6d436c6dd7c9817970864d911e4ac518803901
 $res=$conn->prepare($sql);
 $res->execute();
 $res=$res->fetchAll(PDO::FETCH_ASSOC);
--- conn/conn.php
+++ conn/conn.php
@@ -1,9 +1,9 @@
 <?php
 
 $server = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'ogajara';
+$username = 'u172132148_c';
+$password = '/lh0T4Qs3;';
+$database = 'u172132148_c';
 
 try {
     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
